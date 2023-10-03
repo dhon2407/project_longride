68869d041fff3d297a54384ea72c6932ea5c386e
Dan <jessiedaniel7@yahoo.com>
2023-10-03 11:46:43 +0900

    Bike basic debug controls

---
+++ Assets/Scripts/Bike/CrankController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Bike
+{
+    [HideMonoScript]
+    public class CrankController : MonoBehaviour
+    {
+        [SerializeField, Min(0)] private float rotateSpeed = 10;
+        
+        private Transform _transform;
+        private bool _active;
+
+        private void Awake()
+        {
+            _transform = transform;
+            _active = _transform != null;
+        }
+
+        public void Rotate(float angle)
+        {
+            if (!_active)
+                return;
+            
+            /* Rotate the whole crank clockwise(+angle) */
+            _transform.RotateAround(_transform.position, -_transform.right, angle);
+        }
+
+#if UNITY_EDITOR
+        private void Update()
+        {
+            float input = Input.GetAxisRaw("Vertical");
+            if (input != 0)
+                Rotate(input * rotateSpeed * Time.deltaTime);
+        }
+#endif
+    }
+}
---
+++ Assets/Scripts/Bike/SteererController.cs
@@ -0,0 +1,38 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Bike
+{
+    [HideMonoScript]
+    public class SteererController : MonoBehaviour
+    {
+        [SerializeField, Min(0)] private float rotateSpeed = 100;
+
+        private Transform _transform;
+        private bool _active;
+
+        private void Awake()
+        {
+            _transform = transform;
+            _active = _transform != null;
+        }
+
+        public void Rotate(float angle)
+        {
+            if (!_active)
+                return;
+
+            _transform.RotateAround(_transform.position, _transform.up, angle);
+        }
+
+#if UNITY_EDITOR
+        private void Update()
+        {
+            float input = Input.GetAxisRaw("Horizontal");
+            if (input != 0)
+                Rotate(input * rotateSpeed * Time.deltaTime);
+
+        }
+#endif
+    }
+}
